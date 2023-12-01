e1e3dd7069c65725d6c806f59cbb90bbbb5cf433
Diego Ribeiro <diegoribeiro@alu.ufc.br>
2023-12-01 11:26:48 -0300

    Classe Localização
    implementando a classe

---
+++ src/models/LocalizacaoModel.java
@@ -0,0 +1,51 @@
+package models;
+
+public class LocalizacaoModel {
+    byte unidade, bloco; 
+    String sala; 
+
+    String[] colecaoDeSalas = {"1","2","3","4","5","6","7","8"} ; //Array com as salas de cada bloco.
+    //NÃO ESTÁ DEFINIDO *AINDA* COM EXATIDÃO, POIS É NECESSÁRIO REVERMOS PRECISAMENTE QUAL QUANTIDADE DE SALAS POR CADA BLOCO,
+    //E OS LABORATORIOS AUXILIARES.
+
+    public byte getUnidade(){
+        return this.unidade;
+    }
+    public byte getBloco(){
+        return this.bloco;
+    }
+    public String getSala(){
+        return this.sala;
+    }
+
+    public void setUnidade(byte unidade){ //Irá setar a unidade em que o patrimônio está presente.
+        if (unidade > 0 && unidade < 3) { //Como o campus só tem 2 unidades, so aceitara valor 1 e 2 para setar.
+            this.unidade = unidade;
+        }else{
+            this.unidade = 0;
+        }
+    }
+
+    public void setBloco(byte bloco){ //Setar o bloco existente do patrimônio.
+        if (bloco > 0 && bloco < 3) { //Se o bloco for 1 ou 2, ele irá setar.
+            this.bloco = bloco;
+        }else{ //Se não, não irá setar.
+            this.bloco = 0;
+        }
+    }
+
+    public void setSala(String sala){ //Setando as salas.
+
+        for (String percorredor : colecaoDeSalas) { //Irá percorrer o array, vendo se a sala setada existe no Array.
+
+            if (percorredor.equals(sala)) { //Se encontrar no Array de Salas existentes, irá setar.
+                this.sala = sala;
+                return;
+            }
+        }
+
+        this.sala = null; //Caso não, receberá null.
+
+    }
+
+}
