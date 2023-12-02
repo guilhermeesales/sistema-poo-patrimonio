208819640ab4f8e431afcc717b3a151eef27e002
Victor Ravel <143897726+VictorYates@users.noreply.github.com>
2023-12-02 18:15:55 -0300

    Update BasicController

--- src/controllers/BasicController.java
+++ src/controllers/BasicController.java
@@ -1,13 +1,13 @@
 package controllers;
 
 import java.util.ArrayList;
-import models.ChamadoModel
+import models.ChamadoModel;
 
 
 public class BasicController {
 	ArrayList<Chamado> Chamados = new ArrayList<>();
 	public Chamado criarChamado() { 
-		ChamadoModel chamado = new ChamadaModel();
+		ChamadoModel chamado = new ChamadoModel();
 		chamado.setNomeChamado(nomeChamado);
 		chamado.setTipoChamado(tipoChamado);
 		chamado.setDescricaoChamado(descricaoChamado);
